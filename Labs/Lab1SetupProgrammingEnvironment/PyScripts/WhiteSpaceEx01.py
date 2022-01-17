45f6b937d205840a4810f9cd54116ce5d1985c82
kikoken831 <kennyhu831@gmail.com>
2022-01-17 19:14:40 +0800

    Added all lab files

---
+++ Labs/Lab1SetupProgrammingEnvironment/PyScripts/WhiteSpaceEx01.py
@@ -0,0 +1,4 @@
+longWindedComputation = (1 + 2 + 3 + 4 + 5 + 6 + 13 + 14 + 15 + 16 + 17)
+easierToReadListOfLists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(longWindedComputation)
+print(easierToReadListOfLists)
